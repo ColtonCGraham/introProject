6caca805a0d1463b1d5771f02e8c0d7f32fbd547
ColtonCGraham <coltoncgraham@hotmail.com>
2023-07-10 20:13:33 -0500

    Team player connections comleted.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,4 @@
 class Team < ApplicationRecord
   has_and_belongs_to_many :players
-  has_one :sport
+  belongs_to :sport
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+require 'faker'
+
+Team.destroy_all
+Sport.destroy_all
+Player.destroy_all
+Match.destroy_all
+Location.destroy_all
+
+csv_file = Rails.root.join('db/teams.csv')
+csv_data = File.read(csv_file)
+
+team_names = CSV.parse(csv_data, headers: true)
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+team_names.each do |team|
+  spo = Sport.find_or_create_by(name: team_names['Sport'])
+  Team.create!(name: team['Team Name'],
+               sport: spo)
+end
+
+Team.all.each do |t|
+  3.times do
+    n = [Faker::Name.first_name, Faker::Name.last_name].join(' ')
+    player = Player.find_or_create_by(name: n)
+    t.players << player
+  end
+end
