2ee6cfee2df26224a472b0f23b1af311d783fe41
ColtonCGraham <coltoncgraham@hotmail.com>
2023-07-16 16:28:06 -0500

    added pagination for players dataset.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,7 +3,7 @@ class PlayersController < ApplicationController
 
   # GET /players or /players.json
   def index
-    @players = Player.all
+    @players = Player.order(:name).page params[:page]
   end
 
   # GET /players/1 or /players/1.json
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,4 @@
 class Player < ApplicationRecord
+  paginates_per 50
   has_and_belongs_to_many :teams
 end
